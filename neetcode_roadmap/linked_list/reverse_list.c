5f397754934c298e79e8e77bc85f36260b60f1c4
Kamil Sobierajski <sichparadiesisch@gmail.com>
2025-01-10 23:58:41 +0100

    Solve 'Reverse List' Problem

---
+++ neetcode_roadmap/linked_list/reverse_list.c
@@ -0,0 +1,18 @@
+typedef struct {
+    int val;
+    struct ListNode *next;
+} ListNode;
+
+struct ListNode* reverseList(struct ListNode* head){
+    if(head == NULL || head->next == NULL) return head;
+    struct ListNode* prev = head;
+    struct ListNode* curr = prev->next;
+    head->next = NULL;
+    while(prev != NULL && curr != NULL){
+        struct ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;    
+}
