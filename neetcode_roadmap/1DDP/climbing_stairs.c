ada5e42840657d2bbe45587a1750921aa1e00238
Kamil Sobierajski <sichparadiesisch@gmail.com>
2025-03-12 23:13:30 +0100

    Solve 'Climbing Stairs' Problem

---
+++ neetcode_roadmap/1DDP/climbing_stairs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int climb_stairs(int n) {
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+
+    int prev1 = 1, prev2 = 2, curr;
+    
+    for (int i = 3; i <= n; i++) {
+        curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+    
+    return curr;
+}
